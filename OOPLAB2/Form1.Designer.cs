8c4bcc94051627b24050dd4d1d1c90ab6c2a1f91
mrtsknn <102482801+mrtsknn@users.noreply.github.com>
2022-03-27 21:31:09 +0300

    Initial commit

---
+++ OOPLAB2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace OOPLAB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MFDESIGN = new System.Windows.Forms.PictureBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.emptyUser = new System.Windows.Forms.Label();
+            this.emptyPass = new System.Windows.Forms.Label();
+            this.userLogo = new System.Windows.Forms.PictureBox();
+            this.passLogo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MFDESIGN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MFDESIGN
+            // 
+            this.MFDESIGN.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.MFDESIGN.BackColor = System.Drawing.Color.Transparent;
+            this.MFDESIGN.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MFDESIGN.BackgroundImage")));
+            this.MFDESIGN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MFDESIGN.Location = new System.Drawing.Point(319, 12);
+            this.MFDESIGN.Name = "MFDESIGN";
+            this.MFDESIGN.Size = new System.Drawing.Size(200, 173);
+            this.MFDESIGN.TabIndex = 0;
+            this.MFDESIGN.TabStop = false;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(355, 233);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(150, 20);
+            this.txtuser.TabIndex = 1;
+            this.txtuser.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(355, 309);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(150, 20);
+            this.txtpass.TabIndex = 2;
+            this.txtpass.UseSystemPasswordChar = true;
+            this.txtpass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // emptyUser
+            // 
+            this.emptyUser.AutoSize = true;
+            this.emptyUser.BackColor = System.Drawing.Color.Transparent;
+            this.emptyUser.Font = new System.Drawing.Font("Bauhaus 93", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emptyUser.Location = new System.Drawing.Point(355, 209);
+            this.emptyUser.Name = "emptyUser";
+            this.emptyUser.Size = new System.Drawing.Size(95, 21);
+            this.emptyUser.TabIndex = 3;
+            this.emptyUser.Text = "USERNAME";
+            this.emptyUser.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // emptyPass
+            // 
+            this.emptyPass.AutoSize = true;
+            this.emptyPass.BackColor = System.Drawing.Color.Transparent;
+            this.emptyPass.Font = new System.Drawing.Font("Bauhaus 93", 14.25F);
+            this.emptyPass.Location = new System.Drawing.Point(355, 285);
+            this.emptyPass.Name = "emptyPass";
+            this.emptyPass.Size = new System.Drawing.Size(102, 21);
+            this.emptyPass.TabIndex = 4;
+            this.emptyPass.Text = "PASSWORD";
+            this.emptyPass.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // userLogo
+            // 
+            this.userLogo.BackColor = System.Drawing.Color.Transparent;
+            this.userLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userLogo.BackgroundImage")));
+            this.userLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.userLogo.Location = new System.Drawing.Point(319, 223);
+            this.userLogo.Name = "userLogo";
+            this.userLogo.Size = new System.Drawing.Size(30, 30);
+            this.userLogo.TabIndex = 5;
+            this.userLogo.TabStop = false;
+            this.userLogo.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // passLogo
+            // 
+            this.passLogo.BackColor = System.Drawing.Color.Transparent;
+            this.passLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("passLogo.BackgroundImage")));
+            this.passLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.passLogo.Location = new System.Drawing.Point(319, 299);
+            this.passLogo.Name = "passLogo";
+            this.passLogo.Size = new System.Drawing.Size(30, 30);
+            this.passLogo.TabIndex = 6;
+            this.passLogo.TabStop = false;
+            this.passLogo.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bauhaus 93", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(444, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "LOG IN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(709, 539);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "V1.0.0 Beta";
+            this.label1.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passLogo);
+            this.Controls.Add(this.userLogo);
+            this.Controls.Add(this.emptyPass);
+            this.Controls.Add(this.emptyUser);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.MFDESIGN);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MFDESIGN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MFDESIGN;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label emptyUser;
+        private System.Windows.Forms.Label emptyPass;
+        private System.Windows.Forms.PictureBox userLogo;
+        private System.Windows.Forms.PictureBox passLogo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OOPLAB2/Form2.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace OOPLAB2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtj = new System.Windows.Forms.TextBox();
+            this.txti = new System.Windows.Forms.TextBox();
+            this.rdCstm = new System.Windows.Forms.RadioButton();
+            this.rdHard = new System.Windows.Forms.RadioButton();
+            this.rdMed = new System.Windows.Forms.RadioButton();
+            this.rdEasy = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lblLvl = new System.Windows.Forms.Label();
+            this.lblShp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(414, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 39);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bauhaus 93", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(489, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtj);
+            this.panel1.Controls.Add(this.txti);
+            this.panel1.Controls.Add(this.rdCstm);
+            this.panel1.Controls.Add(this.rdHard);
+            this.panel1.Controls.Add(this.rdMed);
+            this.panel1.Controls.Add(this.rdEasy);
+            this.panel1.Location = new System.Drawing.Point(457, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 166);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "x";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtj
+            // 
+            this.txtj.Location = new System.Drawing.Point(169, 95);
+            this.txtj.Name = "txtj";
+            this.txtj.Size = new System.Drawing.Size(29, 22);
+            this.txtj.TabIndex = 20;
+            // 
+            // txti
+            // 
+            this.txti.Location = new System.Drawing.Point(114, 95);
+            this.txti.Name = "txti";
+            this.txti.Size = new System.Drawing.Size(29, 22);
+            this.txti.TabIndex = 19;
+            // 
+            // rdCstm
+            // 
+            this.rdCstm.AutoSize = true;
+            this.rdCstm.Location = new System.Drawing.Point(32, 96);
+            this.rdCstm.Name = "rdCstm";
+            this.rdCstm.Size = new System.Drawing.Size(76, 21);
+            this.rdCstm.TabIndex = 18;
+            this.rdCstm.TabStop = true;
+            this.rdCstm.Text = "Custom";
+            this.rdCstm.UseVisualStyleBackColor = true;
+            // 
+            // rdHard
+            // 
+            this.rdHard.AutoSize = true;
+            this.rdHard.Location = new System.Drawing.Point(32, 69);
+            this.rdHard.Name = "rdHard";
+            this.rdHard.Size = new System.Drawing.Size(60, 21);
+            this.rdHard.TabIndex = 17;
+            this.rdHard.TabStop = true;
+            this.rdHard.Text = "Hard";
+            this.rdHard.UseVisualStyleBackColor = true;
+            // 
+            // rdMed
+            // 
+            this.rdMed.AutoSize = true;
+            this.rdMed.Location = new System.Drawing.Point(32, 42);
+            this.rdMed.Name = "rdMed";
+            this.rdMed.Size = new System.Drawing.Size(78, 21);
+            this.rdMed.TabIndex = 16;
+            this.rdMed.TabStop = true;
+            this.rdMed.Text = "Medium";
+            this.rdMed.UseVisualStyleBackColor = true;
+            // 
+            // rdEasy
+            // 
+            this.rdEasy.AutoSize = true;
+            this.rdEasy.Location = new System.Drawing.Point(32, 15);
+            this.rdEasy.Name = "rdEasy";
+            this.rdEasy.Size = new System.Drawing.Size(60, 21);
+            this.rdEasy.TabIndex = 15;
+            this.rdEasy.TabStop = true;
+            this.rdEasy.Text = "Easy";
+            this.rdEasy.UseVisualStyleBackColor = true;
+            this.rdEasy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.checkBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Location = new System.Drawing.Point(457, 334);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(174, 141);
+            this.panel2.TabIndex = 16;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(114, 70);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox4.TabIndex = 21;
+            this.pictureBox4.TabStop = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(32, 70);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(72, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Round";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(114, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(114, 43);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox3.TabIndex = 20;
+            this.pictureBox3.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(32, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Square";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(32, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Triangle";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lblLvl
+            // 
+            this.lblLvl.AutoSize = true;
+            this.lblLvl.Font = new System.Drawing.Font("Bauhaus 93", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLvl.Location = new System.Drawing.Point(452, 85);
+            this.lblLvl.Name = "lblLvl";
+            this.lblLvl.Size = new System.Drawing.Size(185, 28);
+            this.lblLvl.TabIndex = 17;
+            this.lblLvl.Text = "Diffuculty Levels";
+            // 
+            // lblShp
+            // 
+            this.lblShp.AutoSize = true;
+            this.lblShp.Font = new System.Drawing.Font("Bauhaus 93", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShp.Location = new System.Drawing.Point(452, 303);
+            this.lblShp.Name = "lblShp";
+            this.lblShp.Size = new System.Drawing.Size(85, 28);
+            this.lblShp.TabIndex = 18;
+            this.lblShp.Text = "Shapes";
+            this.lblShp.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form2
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 690);
+            this.Controls.Add(this.lblShp);
+            this.Controls.Add(this.lblLvl);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rdHard;
+        private System.Windows.Forms.RadioButton rdMed;
+        private System.Windows.Forms.RadioButton rdEasy;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtj;
+        private System.Windows.Forms.TextBox txti;
+        private System.Windows.Forms.RadioButton rdCstm;
+        private System.Windows.Forms.Label lblLvl;
+        private System.Windows.Forms.Label lblShp;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
